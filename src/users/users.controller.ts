0101ecfc25d15a8663761629ef4d3b64a1127998
Vinicius Gobbi <vgobbi.8@gmail.com>
2025-05-19 20:51:00 -0300

    Changes in docker compose files and start implementing users logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  async register(@Body() dto: RegisterUserDto) {
+    return this.usersService.register(dto);
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginUserDto) {
+    return this.usersService.login(dto);
+  }
+}
